bedd2de0ec0a542a366e02f743bef97b88ce67fa
vohanam1803 <xincaiten2001@gmail.com>
2022-05-06 13:09:55 +0700

    a

---
+++ AMNHAC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AMNHAC.Startup))]
+namespace AMNHAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
