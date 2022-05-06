a1048e92fe8eedfa19c1058006f56b09d685ca83
thanhdat011 <101719088+thanhdat011@users.noreply.github.com>
2022-05-06 14:05:43 +0700

    a

---
+++ AMNHAC/Controllers/MyMusicProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMNHAC.Controllers
+{
+    public class MyMusicProfileController : Controller
+    {
+        // GET: MyMusicProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
