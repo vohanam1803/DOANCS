b8b37351aab942f81ff40a4d4cced978c1b7a6e3
vohanam1803 <xincaiten2001@gmail.com>
2022-05-11 14:24:47 +0700

    a

--- AMNHAC/Controllers/HomeController.cs
+++ AMNHAC/Controllers/HomeController.cs
@@ -6,13 +6,32 @@ using System.Web.Mvc;
 
 namespace AMNHAC.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(String timkiem, FormCollection form)
+        {
 
+            timkiem = form["Id"];
+            if (timkiem != "")
+            {
+                return View();
+            }
+            else
+            {
+                return null;
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ AMNHAC/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AMNHAC.Models;
+namespace AMNHAC.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(String timkiem,FormCollection form)
+        {            
+            timkiem = form["Id"] ;
+            if(timkiem != "")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+
+    
+}
