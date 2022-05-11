af378e01612c095d2493d9a946e002471ae287eb
vohanam1803 <xincaiten2001@gmail.com>
2022-05-11 15:57:01 +0700

    a

---
+++ AMNHAC/Controllers/DiscoverController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMNHAC.Controllers
+{
+    public class DiscoverController : Controller
+    {
+        // GET: Discover
+        public ActionResult IndexDiscover()
+        {
+            return View();
+        }
+    }
+}
