5eb4213e960ff987710e3c0f86b8960c7481f100
vohanam1803 <xincaiten2001@gmail.com>
2022-06-08 00:36:28 +0700

    8/6/2022 0h36

--- AMNHAC/Controllers/HomeController.cs
+++ AMNHAC/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace AMNHAC.Controllers
 
             var youtubeService = new YouTubeService(new BaseClientService.Initializer()
             {
-                ApiKey = "AIzaSyDeD4tyKV8uEXYYDnIKYKwSfO_Vd0bkCoo",
+                ApiKey = "AIzaSyDDefFSi5CM8Ns5PyCsAOGa72qqTrKRRIo",
                 ApplicationName = this.GetType().ToString()
             });
 
@@ -83,6 +83,7 @@ namespace AMNHAC.Controllers
             return vk;
         }
         /////////////////
+
     }
 
 
@@ -101,46 +102,20 @@ namespace AMNHAC.Controllers
             person = data.Persons.ToList();
             return person;
         }
-        private async Task<List<Video>> GetVideoAsyncTQ()
-        {
-            int a = 0;
-            test = await searchObject.RunYouTube("火爆的中国抖音音乐 2022");
-            List<Video> video = new List<Video>();
-            video = test;
-            for (var item = 0; item < video.Count; item++)
-            {
-                video[item].vitrivideo = video[item].id + a;
-                video[item].loaivideo = "1" + a;
-                a++;
-            }
-            return video;
 
-        }
-        private async Task<List<Video>> GetVideoAsyncVN()
-        {
-            int a = 0;
-
-            test = await searchObject.RunYouTube("Nhạc Hot Việt Nam 2022");
-            List<Video> video = new List<Video>();
-            video = test;
-            for (var item = 0; item < video.Count; item++)
-            {
-                video[item].vitrivideo = video[item].id + a;
-                video[item].loaivideo = "2" + a;
-                a++;
-            }
-            return video;
-        }
         //
         [HttpGet]
 
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
 
             dynamic mymodel = new ExpandoObject();
             mymodel.person = GetPerson();
-            mymodel.videoTQ = await GetVideoAsyncTQ();
-            mymodel.videoVN = await GetVideoAsyncVN();
+
+
+            mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+            mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
             return View(mymodel);
         }
 
@@ -212,7 +187,6 @@ namespace AMNHAC.Controllers
 
             for (var item = 0; item < test.Count; item++)
             {
-
                 vs[item].title = test[item].title;
                 vs[item].id = test[item].id;
                 vs[item].author = test[item].author;
@@ -234,16 +208,16 @@ namespace AMNHAC.Controllers
             data.SubmitChanges();
             //Sổ danh sách 
             var all_list = data.Videos.ToList();
-
+            var all_check = from ss in data.Videos where ss.loaivideo != "user" select ss;
             if (all_list.Count == 0)
             {
                 ViewBag.Message = "You Not Have Anything In Playlist";
-                return View(all_list);
+                return View(all_check);
             }
             else
             {
                 ViewBag.Message = "Your Playlist";
-                return View(all_list);
+                return View("~/Views/MyMusicProfile/Index.cshtml", all_check);
             }
         }
 
@@ -272,7 +246,7 @@ namespace AMNHAC.Controllers
             }
         }
         [HttpPost]
-        public async Task<ActionResult> AddPlaylist(FormCollection form)
+        public ActionResult AddPlaylist(FormCollection form)
         {
             List<Video> vs = new List<Video>();
             var vk = new Video();
@@ -280,9 +254,9 @@ namespace AMNHAC.Controllers
 
             //Get id bài hát kiểm tra có bị trùng trong data ko
             vk.id = form["CheckId"];
-            var checkId = data.Videos.Where(m => m.id == vk.id).FirstOrDefault();
+            var checkId = data.Videos.Where(m => m.id == vk.id ).FirstOrDefault();
 
-            test = await GetVideoAsyncTQ();
+            test = data.Videos.ToList();
             vs = new List<Video>(test);
 
 
@@ -293,7 +267,7 @@ namespace AMNHAC.Controllers
                 vs[item].id = test[item].id;
                 vs[item].author = test[item].author;
                 vs[item].link = test[item].link;
-                vs[item].loaivideo = "admin";
+                vs[item].loaivideo = test[item].loaivideo;
                 if (checkId == default)
                 {
                     if (vk.id == vs[item].id)
@@ -316,47 +290,252 @@ namespace AMNHAC.Controllers
 
         }
 
+
         public ActionResult TrangAdmin()
         {
-           
+
             return View();
         }
 
-        [HttpGet]
+
         public ActionResult TrangChu()
         {
-            List<Video> acc = new List<Video>();
-            return View("~/Views/Home/TrangChu.cshtml", acc);
-        }
-        [HttpPost]
-        public async Task<ActionResult> TrangChu(FormCollection form)
-        {
 
+            ViewBag.Message = "You Can Search AnyThing Add Your Playlist And Controll Trang Chủ!!";
+            /*var trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;*/
+            dynamic mymodel = new ExpandoObject();
+            mymodel.person = GetPerson();
+
+            mymodel.trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;
+            mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+            mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+            return View(mymodel);
+            /*return View(trangchu);*/
+
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> AddPlaylistAdmin(FormCollection form)
+        {
+            int a = 0;
+            List<Video> vs = new List<Video>();
             var vk = new Video();
+            //Get tên bài hát muốn thêm vào
             vk.title = form["Id"];
+
+            //Get id bài hát kiểm tra có bị trùng trong data ko
+            vk.id = form["CheckId"];
+            var checkId = data.Videos.Where(m => m.id == vk.id).FirstOrDefault();
+
             //Youtube API
             test = await searchObject.RunYouTube(vk.title);
+            vs = new List<Video>(test);
 
-            if (vk.title != "")
+            //Check thể loại video
+            var check = new Theloai();
+            check.nameTheloai = form["theloai"];
+
+
+
+            for (var item = 0; item < test.Count; item++)
             {
-                if (test.Count == 0)
+                vs[item].id = test[item].id;
+                vs[item].title = test[item].title;
+                vs[item].author = test[item].author;
+                vs[item].link = test[item].link;
+                vs[item].vitrivideo = vs[item].id + a;
+                vs[item].loaivideo = "admin" + vs[item].id;
+                if (check.nameTheloai == "hoa")
                 {
-                    ViewBag.Message = "Can't find!!!!!";
-                    return View(test);
+                    vs[item].idTheloai = check.idTheloai = 1;
+
+                }
+                if (check.nameTheloai == "viet")
+                {
+                    vs[item].idTheloai = check.idTheloai = 2;
+
+                }
+                if (check.nameTheloai == "khac")
+                {
+                    vs[item].idTheloai = check.idTheloai = 4;
+
+                }
+                a++;
+                if (checkId == default)
+                {
+                    if (vk.title == vs[item].title)
+                    {
+                        data.Videos.InsertOnSubmit(vs[item]);
+                    }
                 }
                 else
                 {
-                    ViewBag.Message = "Your Search Results!!";
-                    return View(test);
-                }
+                    ViewBag.Message = "This Have In Your PlayList !!";
+                    dynamic mymodel = new ExpandoObject();
+                    mymodel.person = GetPerson();
 
+                    mymodel.trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;
+                    mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+                    mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+                    return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                   /* ViewBag.Message = "This Have In Your PlayList !!";
+                    return View("~/Views/Home/TrangChu.cshtml", test);*/
+                }
+            }
+            data.SubmitChanges();
+            //Sổ danh sách 
+
+            var all_list = data.Videos.ToList();
+
+            /*var all_sach = from ss in data.Videos where ss.loaivideo != "user" select ss;*/
+
+            if (all_list.Count == 0)
+            {
+                ViewBag.Message = "You Not Have Anything In Playlist";
+                dynamic mymodel = new ExpandoObject();
+                mymodel.person = GetPerson();
+
+                mymodel.trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;
+                mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+                mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+                return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                /*ViewBag.Message = "You Not Have Anything In Playlist";
+                return View("~/Views/Home/TrangChu.cshtml", all_sach);*/
             }
             else
             {
+                ViewBag.Message = "Your Playlist";
+                dynamic mymodel = new ExpandoObject();
+                mymodel.person = GetPerson();
 
-                return View("~/Views/Home/TrangChu.cshtml");
+                mymodel.trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;
+                mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+                mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+                return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                /*ViewBag.Message = "Your Playlist";
+                return View("~/Views/Home/TrangChu.cshtml", all_sach);*/
             }
-
         }
+
+
+
+        /// <summary>
+        /// //////////
+        /// </summary>
+        /// <returns></returns>
+        /*private async Task<List<Video>> GetVideoAsyncTQ()
+        {
+            int a = 0;
+
+            test = await searchObject.RunYouTube("Nhạc Hot Trung Quốc TikTokS 2022");
+            List<Video> video = new List<Video>();
+            video = test;
+            for (var item = 0; item < video.Count; item++)
+            {
+                video[item].vitrivideo = video[item].id + a;
+                video[item].loaivideo = "1" + a;
+                a++;
+            }
+            return video;
+        }*/
+        /*private async Task<List<Video>> GetVideoAsyncVN()
+        {
+            int a = 0;
+
+            test = await searchObject.RunYouTube("Nhạc Hot Việt Nam 2022");
+            List<Video> video = new List<Video>();
+            video = test;
+            for (var item = 0; item < video.Count; item++)
+            {
+                video[item].vitrivideo = video[item].id + a;
+                video[item].loaivideo = "2" + a;
+                a++;
+            }
+            return video;
+        }*/
+        [HttpPost]
+        public ActionResult DetelePlaylistAdmin(string id, FormCollection collection)
+        {
+            var D_playlist = data.Videos.Where(m => m.id == id).First();
+            data.Videos.DeleteOnSubmit(D_playlist);
+            data.SubmitChanges();
+            var AfterD = data.Videos.ToList();
+            /*var all_sach = from ss in data.Videos where ss.loaivideo != "user" select ss;*/
+
+
+            dynamic mymodel = new ExpandoObject();
+            mymodel.person = GetPerson();
+
+            mymodel.trangchu = from ss in data.Videos where ss.loaivideo != "user" select ss;
+            mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+            mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+            
+            for(var item = 0;item < AfterD.Count;item++)
+            {
+                if(AfterD[item].loaivideo != "user")
+                {
+                    ViewBag.Message = "Your Playlist";
+                    return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                }
+                
+            }
+            ViewBag.Message = "You Not Have Anything In Playlist";
+            return View("~/Views/Home/TrangChu.cshtml", mymodel);
+            /*if (AfterD.Count == 0)
+            {
+                ViewBag.Message = "You Not Have Anything In Playlist";
+                return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                *//* return View("~/Views/Home/TrangChu.cshtml", all_sach);*//*
+            }
+            else
+            {
+                ViewBag.Message = "Your Playlist";
+                return View("~/Views/Home/TrangChu.cshtml", mymodel);
+                *//*return View("~/Views/Home/TrangChu.cshtml", all_sach);*//*
+            }*/
+        }
+        [HttpPost]
+        public ActionResult SearchInPlaylist(FormCollection collection)
+        {
+            
+            string timkiem = collection["SearchInPlaylist"];
+            var D_playlist = data.Videos.ToList();
+            /*var list = new List<Video>(D_playlist.Count);*/
+            for (var item = 0; item < D_playlist.Count; item++)
+            {
+                if (D_playlist[item].title.Contains(timkiem))
+                {
+                    D_playlist[item].vitrivideo = "1";
+                    /*list[item].id = D_playlist[item].id;
+                    list[item].title = D_playlist[item].title;
+                    list[item].author = D_playlist[item].author;
+                    list[item].link = D_playlist[item].link;
+                    list[item].vitrivideo = D_playlist[item].vitrivideo;
+                    list[item].loaivideo = D_playlist[item].loaivideo;
+                    list[item].idTheloai = D_playlist[item].idTheloai;*/
+                }
+            }
+            var list = from ss in D_playlist where ss.vitrivideo == "1" && ss.loaivideo != "user" select ss;
+
+            dynamic mymodel = new ExpandoObject();
+            mymodel.person = GetPerson();
+
+            mymodel.trangchu = list;
+            mymodel.videoTQ = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 1 select ss;
+
+            mymodel.videoVN = from ss in data.Videos where ss.loaivideo != "user" && ss.idTheloai == 2 select ss;
+            return View("~/Views/Home/TrangChu.cshtml",mymodel);
+            /*return View("~/Views/Home/TrangChu.cshtml", list);*/
+        }
+
+
+        
+
     }
 }
